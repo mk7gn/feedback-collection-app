b4c0cc6a0ae254d989037fd68a8d55667a7a4b55
Maksym Goncharov <maksym.g@stms.co.il>
2020-05-22 17:29:37 +0300

    add header

---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="/" className="brand-logo">Emaily</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="/auth/google">Login with Google</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
