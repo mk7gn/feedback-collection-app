7c83e0f8b4babe0a0e3e46e6a8b7a66ba6c17bed
Maksym Goncharov <maksym.g@stms.co.il>
2020-05-25 11:47:42 +0300

    improve auth flow

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-export default class Header extends Component {
+class Header extends Component {
+  renderContent() {
+    const {auth} = this.props;
+    if (auth === null) {
+      return;
+    }
+    if (auth === false) {
+      return (
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a href="/auth/google">Login with Google</a></li>
+        </ul>
+      );
+    }
+    return (
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li>{auth.firstName} {auth.lastName}</li>
+        <li><a href="/auth/logout">Logout</a></li>
+      </ul>
+    );
+  }
+
   render() {
+    console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="/" className="brand-logo">Emaily</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="/auth/google">Login with Google</a></li>
-          </ul>
+          <Link to={this.props.auth ? '/surveys' : '/'}
+                className="brand-logo">
+                Emaily
+          </Link>
+          { this.renderContent() }
         </div>
       </nav>
     );
   }
 }
+
+function mapStateToProps({auth}) {
+  return { auth };
+};
+
+export default connect(mapStateToProps)(Header);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,14 +10,15 @@ module.exports = (app) => {
 
   app.get(
     '/auth/google/callback',
-    passport.authenticate('google')
+    passport.authenticate('google'),
+    (req, res) => res.redirect('/surveys')
   );
 
   app.get(
     '/auth/logout',
     (req, res) => {
       req.logout();
-      res.send(req.user);
+      res.redirect('/');
     }
   );
 
